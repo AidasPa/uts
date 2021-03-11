2b02dfee9c0e97dcc8aa0c3deafe30e63dd1ae56
Aidas Patas <aidas0330@gmail.com>
2021-03-11 17:41:58 +0200

    remove typings folder in favour of stubs

--- typings/_part_3_ue.d.ts.ts
+++
@@ -1,7 +0,0 @@
-type UPROPERTYParam = null;
-
-declare const EditAnywhere: UPROPERTYParam = null;
-
-declare const UCLASS = (): Function => (): void => {};
-declare const UPROPERTY = (...args: UPROPERTYParam[]): Function => (): void => {};
-declare const UFUNCTION = (...args: UPROPERTYParam[]): Function => (): void => {};
