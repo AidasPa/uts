24e7fa5cf78402630b871c497564cb55bf9cc832
Aidas Patas <aidas0330@gmail.com>
2021-03-11 20:22:26 +0200

    move watch action to its own file

--- cli/actions.js
+++ cli/actions.js
@@ -1,72 +1,10 @@
 const colors = require('colors');
-const filewatcher = require('filewatcher');
-const fs = require('fs').promises;
-const compiler = require('../compiler');
-const typescript = require('../compiler/typescript');
-const utils = require('../utils');
+const watch = require('./actions/watch');
+
 const make = require('./stubs/make');
 
-const removeFileFromPathString = (path) => {
-  const split = path.split('/');
-  split.pop();
-  return split.join('/');
-};
-
 module.exports = {
-  watch: () => {
-    // eslint-disable-next-line global-require
-    const glob = require('glob');
-
-    const watcher = filewatcher();
-    const typescriptWatcher = filewatcher();
-
-    glob('**/*.ts', (err, matches) => {
-      matches.forEach((match) => typescriptWatcher.add(match));
-    });
-
-    glob('**/*.uts', (err, matches) => {
-      matches.forEach((match) => watcher.add(match));
-    });
-
-    console.log(`[${colors.bold.magenta('UTS')}] Watching ${colors.cyan.italic('.uts files')}...`);
-    console.log(`[${colors.bold.cyan('TS')}] Watching ${colors.cyan.italic('.ts files')}...`);
-
-    typescriptWatcher.on('change', async (file, stat) => {
-      if (stat) {
-        console.log(`[${colors.bold.cyan('TS')}] Compiling ${colors.cyan.italic(file)}...`);
-        const code = await utils.readCode(file);
-        const compiled = typescript(code);
-
-        try {
-          await fs.mkdir(`out/${removeFileFromPathString(file)}`, { recursive: true });
-        } catch (error) {
-          // silent
-        }
-
-        utils.writeCode(`out/${file}`, compiled);
-      } else {
-        fs.unlink(`out/${file}`);
-      }
-    });
-
-    watcher.on('change', async (file, stat) => {
-      if (stat) {
-        console.log(`[${colors.bold.magenta('UTS')}] Compiling ${colors.cyan.italic(file)}...`);
-        const code = await utils.readCode(file);
-        const compiled = compiler(code);
-
-        try {
-          await fs.mkdir(`out/${removeFileFromPathString(file)}`, { recursive: true });
-        } catch (error) {
-          // silent
-        }
-
-        utils.writeCode(`out/${file}`, compiled);
-      } else {
-        fs.unlink(`out/${file}`);
-      }
-    });
-  },
+  watch,
   init: async () => {
     // create typings for decorators
     make('typings', [], 'typings/_part_3_ue.d.ts');
