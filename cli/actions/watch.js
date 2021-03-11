c1184c6e533000e0b0151975923d2ff9e1c155dc
Aidas Patas <aidas0330@gmail.com>
2021-03-11 23:03:17 +0200

    fix watcher delete

--- cli/actions/watch.js
+++ cli/actions/watch.js
@@ -1,4 +1,5 @@
 const chokidar = require('chokidar');
+const fs = require('fs').promises;
 const cli = require('../cli');
 const compiler = require('../../compiler');
 const typescript = require('../../compiler/typescript');
@@ -19,12 +20,17 @@ module.exports = () => {
   chokidar.watch('src').on('all', async (event, path) => {
     const file = path.replace(/\\/g, '/');
 
+    const target = `out/${utils.getPathDirs(file, true)}`;
+
     if (file.split('.')[1] === 'uts' || file.split('.')[1] === 'ts') {
+      if (event === 'unlink') {
+        return fs.unlink(`${target}/${utils.getLastPathPart(file.split('.')[0])}.js`);
+      }
       const fileType = file.split('.')[1];
       const code = await utils.readCode(file);
 
       try {
-        await utils.createDirsRecursive(`out/${utils.getPathDirs(file, true)}`);
+        await utils.createDirsRecursive(target);
       } catch (error) {
         // silently fail
       }
@@ -32,7 +38,7 @@ module.exports = () => {
       let compiled;
 
       if (fileType === 'uts') {
-        cli.utsCompiling(file);
+        cli.utsCompiling(`${file}`);
         compiled = compiler(code);
       } else if (fileType === 'ts') {
         cli.tsCompiling(file);
