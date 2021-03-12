79cd4ffedcda2fcb0c51dada34de988dae73aa85
Aidas Patas <aidas0330@gmail.com>
2021-03-12 11:22:06 +0200

    uts init now works with yarn

--- cli/actions.js
+++ cli/actions.js
@@ -1,35 +1,12 @@
-const fs = require('fs').promises;
 const watch = require('./actions/watch');
+const init = require('./actions/init');
 const cli = require('./cli');
 const utils = require('../utils');
 const make = require('./stubs/make');
 
 module.exports = {
   watch,
-  init: async () => {
-    // create typings for decorators
-    make('typings', [], 'typings/_part_3_ue.d.ts', true);
-
-    // make sure to import the file to the base
-    try {
-      const mainTypingFile = await fs.readFile('typings/ue.d.ts', 'utf-8');
-      const splitTypingFile = mainTypingFile.split('\n');
-
-      let injectionTarget = null;
-      splitTypingFile.forEach((line, i) => {
-        if (line === '/// <reference path="_part_2_ue.d.ts">/>') {
-          injectionTarget = i + 1;
-        }
-      });
-
-      splitTypingFile.splice(injectionTarget, 0, ...['/// <reference path="_part_3_ue.d.ts">/>']);
-      await fs.writeFile('typings/ue.d.ts', splitTypingFile.join('\n'), 'utf-8');
-
-      cli.utsInitialized();
-    } catch (error) {
-      cli.warning('Are you sure this is an UnrealJS project?\n\nDid you setup UnrealJS in your project\'s plugins?');
-    }
-  },
+  init,
   'create:actor': async (className) => {
     make('actor', [['CLASSNAME', utils.getLastPathPart(className)]], `${className}.uts`);
     cli.actorCreated(`${className}.uts`);
---
+++ cli/actions/init.js
@@ -0,0 +1,60 @@
+const fs = require('fs').promises;
+const execa = require('execa');
+const Listr = require('listr');
+const cli = require('../cli');
+const make = require('../stubs/make');
+
+// eslint-disable-next-line consistent-return
+module.exports = async () => {
+  // check if maybe already exists
+  // eslint-disable-next-line global-require
+  if (require('fs').existsSync('typings/_part_3_ue.d.ts')) {
+    return cli.warning('It seems like the project has already been initialized?');
+  }
+
+  // create typings for decorators
+  make('typings', [], 'typings/_part_3_ue.d.ts', true);
+
+  // make sure to import the file to the base
+  try {
+    const mainTypingFile = await fs.readFile('typings/ue.d.ts', 'utf-8');
+    const splitTypingFile = mainTypingFile.split('\n');
+
+    let injectionTarget = null;
+    splitTypingFile.forEach((line, i) => {
+      if (line === '/// <reference path="_part_2_ue.d.ts">/>') {
+        injectionTarget = i + 1;
+      }
+    });
+
+    splitTypingFile.splice(injectionTarget, 0, ...['/// <reference path="_part_3_ue.d.ts">/>']);
+    await fs.writeFile('typings/ue.d.ts', splitTypingFile.join('\n'), 'utf-8');
+  } catch (error) {
+    cli.warning('Are you sure this is an UnrealJS project?\n\nDid you setup UnrealJS in your project\'s plugins?');
+  }
+
+  const tasks = new Listr([
+    {
+      title: 'Initialize yarn project',
+      task: (ctx, task) => execa('yarn init -y')
+        .catch(() => {
+          ctx.yarn = false;
+
+          task.skip('Yarn not available, install it via `npm install -g yarn`');
+        }),
+    },
+    {
+      title: 'Install package dependencies with Yarn',
+      task: (ctx, task) => execa('yarn add @types/node')
+        .catch((err) => {
+          ctx.yarn = false;
+          console.log(err);
+
+          task.skip('Yarn not available, install it via `npm install -g yarn`');
+        }),
+    },
+  ]);
+
+  await tasks.run();
+  cli.utsInitialized();
+};
