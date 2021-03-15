d3cdd77b1d6fa158934e7012f3b0cda4086304f0
Aidas Patas <aidas0330@gmail.com>
2021-03-15 15:03:16 +0200

    add watch script when init

--- cli/actions.js
+++ cli/actions.js
@@ -1,11 +1,9 @@
-const watch = require('./actions/watch');
 const init = require('./actions/init');
 const cli = require('./cli');
 const utils = require('../utils');
 const make = require('./stubs/make');
 
 module.exports = {
-  watch,
   init,
   'create:actor': async (className) => {
     make('actor', [['CLASSNAME', utils.getLastPathPart(className)]], `${className}.u.ts`);
--- cli/actions/init.js
+++ cli/actions/init.js
@@ -4,14 +4,10 @@ const Listr = require('listr');
 const cli = require('../cli');
 const make = require('../stubs/make');
 
+const babelDevWatch = 'babel src --out-dir out --copy-files --extensions ".ts" -w';
+
 // eslint-disable-next-line consistent-return
 module.exports = async () => {
-  // check if maybe already exists
-  // eslint-disable-next-line global-require
-  if (require('fs').existsSync('typings/_part_3_ue.d.ts')) {
-    return cli.warning('It seems like the project has already been initialized?');
-  }
-
   // create typings for decorators
   make('typings', [], 'typings/_part_3_ue.d.ts', true);
 
@@ -20,49 +16,72 @@ module.exports = async () => {
     const mainTypingFile = await fs.readFile('typings/ue.d.ts', 'utf-8');
     const splitTypingFile = mainTypingFile.split('\n');
 
+    let alreadyInjected = false;
     let injectionTarget = null;
     splitTypingFile.forEach((line, i) => {
+      if (line === '/// <reference path="_part_3_ue.d.ts">/>') {
+        alreadyInjected = true;
+      }
       if (line === '/// <reference path="_part_2_ue.d.ts">/>') {
         injectionTarget = i + 1;
       }
     });
 
-    splitTypingFile.splice(injectionTarget, 0, ...['/// <reference path="_part_3_ue.d.ts">/>']);
+    if (!alreadyInjected) {
+      splitTypingFile.splice(
+        injectionTarget,
+        0,
+        ...['/// <reference path="_part_3_ue.d.ts">/>'],
+      );
+    }
     await fs.writeFile('typings/ue.d.ts', splitTypingFile.join('\n'), 'utf-8');
   } catch (error) {
-    cli.warning('Are you sure this is an UnrealJS project?\n\nDid you setup UnrealJS in your project\'s plugins?');
+    cli.warning(
+      "Are you sure this is an UnrealJS project?\n\nDid you setup UnrealJS in your project's plugins?",
+    );
   }
 
   const tasks = new Listr([
     {
-      title: 'Initialize Tarn project',
-      task: (ctx, task) => execa('yarn init -y')
-        .catch(() => {
-          ctx.yarn = false;
-
-          task.skip('Yarn not available, install it via `npm install -g yarn`');
-        }),
+      title: 'Initialize Yarn project',
+      task: (ctx, task) => execa(
+        'yarn init -y',
+      ).catch(() => {
+        ctx.yarn = false;
+        task.skip('Yarn not available, install it via `npm install -g yarn`');
+      }),
+    },
+    {
+      title: 'Add watch script to package.json',
+      task: (ctx, task) => execa(
+        `npx npm-add-script -k watch -v "${babelDevWatch}"`,
+      ).catch(() => {
+        ctx.yarn = false;
+        task.skip('Maybe it is already there?');
+      }),
     },
     {
       title: 'Install package dependencies with Yarn',
-      task: (ctx, task) => execa('yarn add @types/node @babel/cli @babel/core @aidaspa/babel-plugin-uts @babel/plugin-proposal-decorators @babel/preset-env @babel/preset-typescript')
-        .catch(() => {
-          ctx.yarn = false;
+      task: (ctx, task) => execa(
+        'yarn add @types/node @babel/cli @babel/core @aidaspa/babel-plugin-uts @babel/plugin-proposal-decorators @babel/preset-env @babel/preset-typescript',
+      ).catch(() => {
+        ctx.yarn = false;
 
-          task.skip('Yarn not available, install it via `npm install -g yarn`');
-        }),
+        task.skip('Yarn not available, install it via `npm install -g yarn`');
+      }),
     },
     {
       title: 'Initialize Typescript configuration',
-      task: (ctx) => execa('npx tsc --init -experimentalDecorators true --target es6')
-        .catch(() => {
+      task: (ctx) => execa('npx tsc --init -experimentalDecorators true --target es6').catch(
+        () => {
           ctx.tsc = false;
-        }),
+        },
+      ),
     },
   ]);
 
   await make('.gitignore', [], '.gitignore', true);
-  await make('.babelrc', [], '.gitignore', true);
+  await make('.babelrc', [], '.babelrc', true);
 
   await tasks.run();
   cli.utsInitialized();
