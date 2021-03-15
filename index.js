10640acb59acdcff6dce34bd2c147ff712c910c7
Aidas Patas <aidas0330@gmail.com>
2021-03-15 16:05:03 +0200

    update stub with caveat message

--- index.js
+++ index.js
@@ -3,18 +3,8 @@
 /* eslint-disable no-console */
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
-const utils = require('./utils');
-const compiler = require('./compiler');
 const cli = require('./cli');
 
 const { argv } = yargs(hideBin(process.argv));
 
-if (argv.file) {
-  (async () => {
-    const code = await utils.readCode(argv.file);
-    const compiled = compiler(code);
-    console.log(compiled);
-  })();
-} else {
-  cli(argv._);
-}
+cli(argv._);
